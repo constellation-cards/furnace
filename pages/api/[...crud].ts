269959dd438d81ab6ceeac5366013539be5d72c6
Bill Garrett <garrett@astralfrontier.org>
2023-01-20 17:40:50 -0800

    Add security check in CRUD controller

--- pages/api/[...crud].ts
+++ pages/api/[...crud].ts
@@ -1,82 +1,108 @@
-import { omit } from "ramda"
+import { omit } from "ramda";
 
-import type { NextApiRequest, NextApiResponse } from 'next'
-// import { getSession } from "next-auth/react"
-import { PrismaClient } from '@prisma/client'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/react";
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    try {
-        await prisma.$connect()
+type SecurityCheck = (req: NextApiRequest, res: NextApiResponse<any>) => Promise<void>
 
-        const [model, uid] = req.query.crud as string[]
+async function sessionSecurityCheck(req: NextApiRequest, res: NextApiResponse) {
+  const session = await getSession({ req });
+  if (!session) {
+    res.status(401).send("Unauthorized");
+    throw new Error("UnauthorizedError");
+  }
+  // TODO: verify that the user can access the resource in question?
+}
 
-        let controller, include = {}
-        switch (model) {
-            case "deck":
-                controller = prisma.constellationCardDeck
-                break;
-            case "stack":
-                controller = prisma.constellationCardStack
-                break;
-            case "card":
-                controller = prisma.constellationCard
-                break;
-            case "preset":
-                controller = prisma.constellationCardPreset
-                include = { sources: true }
-                break;
-            case "presetSource":
-                controller = prisma.constellationCardPresetSource
-                include = { preset: true }
-                break;
-            case "session":
-                controller = prisma.gameSession
-                break;
-            default:
-                res.status(404).json({err: "Not Found"})
-                return;
-        }
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    await prisma.$connect();
 
-        let result;
+    const [model, uid] = req.query.crud as string[];
 
-        // TODO: include preset sources in preset updates
+    let controller,
+      include = {},
+      securityCheck: SecurityCheck | undefined = sessionSecurityCheck;
 
-        // if (req.method != "GET") {
-        //   const session = await getSession({ req })
-        //   if (!session) {
-        //     res.status(401).send('Unauthorized')
-        //     throw new Error('UnauthorizedError')
-        //   }
-        // }
-
-        switch (req.method) {
-            case "GET":
-                if (uid) {
-                    result = await controller.findUnique({where: {uid}, include})
-                } else {
-                    result = await controller.findMany({include})
-                }
-                break;
-            case "POST":
-                result = await controller.create({data: omit(['uid'], req.body), include})
-                break;
-            case "PUT":
-                result = await controller.update({where: {uid}, data: omit(['uid'], req.body), include})
-                break;
-            case "DELETE":
-                result = await controller.delete({where: {uid}, include})
-                break;
-            default:
-                throw new Error("Not Implemented")
-        }
-
-        res.status(200).json(result)
-    } catch (err) {
-        console.error(err)
-        res.status(500).json({err})
-    } finally {
-        await prisma.$disconnect()
+    switch (model) {
+      case "deck":
+        controller = prisma.constellationCardDeck;
+        break;
+      case "stack":
+        controller = prisma.constellationCardStack;
+        break;
+      case "card":
+        controller = prisma.constellationCard;
+        break;
+      case "preset":
+        controller = prisma.constellationCardPreset;
+        include = { sources: true };
+        break;
+      case "presetSource":
+        controller = prisma.constellationCardPresetSource;
+        include = { preset: true };
+        break;
+      case "session":
+        controller = prisma.gameSession;
+        break;
+      default:
+        res.status(404).json({ err: "Not Found" });
+        return;
     }
+
+    let result;
+
+    // TODO: include preset sources in preset updates
+
+    if (req.method == "GET") {
+      securityCheck = undefined;
+    }
+
+    // TODO: check JWT token for session controller, as they won't have a normal session?
+
+    if (securityCheck) {
+        securityCheck(req, res)
+    }
+
+    switch (req.method) {
+      case "GET":
+        if (uid) {
+          result = await controller.findUnique({ where: { uid }, include });
+        } else {
+          result = await controller.findMany({ include });
+        }
+        break;
+      case "POST":
+        result = await controller.create({
+          data: omit(["uid"], req.body),
+          include
+        });
+        break;
+      case "PUT":
+        result = await controller.update({
+          where: { uid },
+          data: omit(["uid"], req.body),
+          include
+        });
+        break;
+      case "DELETE":
+        result = await controller.delete({ where: { uid }, include });
+        break;
+      default:
+        throw new Error("Not Implemented");
+    }
+
+    res.status(200).json(result);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ err });
+  } finally {
+    await prisma.$disconnect();
+  }
 }
