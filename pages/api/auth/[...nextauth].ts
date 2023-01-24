a4977f43f53f82e604ba0a695ccfc1b8ab7da565
Bill Garrett <garrett@astralfrontier.org>
2023-01-23 20:19:50 -0800

    Add user's ID to session object

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,11 +1,27 @@
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import NextAuth from 'next-auth'
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import NextAuth from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 
-import prisma from "../../../components/prisma-client"
+import prisma from "../../../components/prisma-client";
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
+  callbacks: {
+    async jwt({ token, user, account, profile, isNewUser }) {
+      user && (token.user = user);
+      return token;
+    },
+    async session({ session, token, user }) {
+      session = {
+        ...session,
+        user: {
+          id: user.id,
+          ...session.user
+        }
+      };
+      return session;
+    }
+  },
   providers: [
     DiscordProvider({
       clientId: process.env.DISCORD_CLIENT_ID || "",
@@ -13,7 +29,7 @@ export default NextAuth({
     })
   ],
   session: {
-    strategy: 'database'
-  },
+    strategy: "database"
+  }
   // pages: {} - see https://next-auth.js.org/configuration/options
-})
+});
